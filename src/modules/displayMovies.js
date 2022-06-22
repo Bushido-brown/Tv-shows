7a4335771029e9603ed3f0a3c9fd25e41c31e8c0
officialesse <uzomaesse@gmail.com>
2022-06-22 21:13:48 +0100

    added functions

--- src/modules/displayMovies.js
+++ src/modules/displayMovies.js
@@ -1,11 +1,15 @@
 import { updateLikes, setLike } from './likeFunctionality.js';
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlayy = document.getElementById('overlayy');
+const modalContent = document.querySelector('.modalData');
 
 const displayMovies = async (movieList, appId, section) => {
   const movieSection = document.querySelector(`#${section}-section`);
   movieSection.innerHTML = '';
   movieList.forEach((item) => {
     const mainDiv = document.createElement('div');
-    mainDiv.className = 'movie-wrapper';
+    mainDiv.className = 'movie-wrapper ';
     mainDiv.id = `${section}${item.id}`;
     const details = document.createElement('div');
     details.className = 'movie-wrapper__title';
@@ -24,7 +28,8 @@ const displayMovies = async (movieList, appId, section) => {
     const commentButton = document.createElement('button');
     commentButton.type = 'button';
     commentButton.innerHTML = 'Comments';
-    commentButton.className = 'movie-wrapper__comment-button';
+    commentButton.className = 'movie-wrapper__comment-button ';
+    commentButton.id='open-modal'
     mainDiv.append(img, details, commentButton);
     const reserveButton = document.createElement('button');
     reserveButton.type = 'button';
@@ -40,5 +45,49 @@ const displayMovies = async (movieList, appId, section) => {
   });
   updateLikes(appId);
 };
+
+
+
+let openModal =(modal)=>{
+  if(modal== null) return;
+  modal.classList.add('active')
+  overlayy.classList.add('active');
+}
+const  closeModal =(modal)=>{
+  if(modal== null) return;
+  modal.classList.remove('active')
+  overlayy.classList.remove('active');
+}
+
+openModalButtons.forEach((button) =>{
+  button.addEventListener('click', (e)=>{
+    const foundIndex=Array.from(openModalButtons).indexOf(e.target);
+
+      //  movieList.forEach((item )=>{
+
+
+        modalContent.innerHTML=`
+       
+        <div >  <p> ${ console.log('hello') }</p> </div>
+        `
+       const modal = document.querySelector('button.dataset.modalTarget')
+       openModal(modal)
+  })
+})
+
+// overlayy.addEventListener('click', ()=>{
+//   const  modals = document.querySelectorAll('.modal.active');
+//   modals.forEach((modal)=>{
+//     closeModal(modal)
+//   })
+// })
+
+//  closeModalButtons.forEach((button)=>{
+//    button.addEventListener('click', ()=>{
+//      const modal = button.closest('.modal');
+//      closeModal(modal)
+//    })
+//  })
+
 
 export { displayMovies as default };
---
+++ src/modules/newmodel.js
@@ -0,0 +1,33 @@
+ export  const showModal = ( movieList, openButton, modalContent) =>{
+    const openBtn = document.getElementById(openButton),
+    modalContainer = document.getElementById(modalContent)
+    
+    if(openBtn && modalContainer){
+        openBtn.addEventListener('click', ()=>{
+            modalContainer.classList.add('show-modal')
+        })
+    }
+}
+
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal(){
+    const modalContainer = document.getElementById('modal-container')
+    modalContainer.classList.remove('show-modal')
+}
+closeBtn.forEach(c => c.addEventListener('click', closeModal))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
